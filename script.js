7aa463f31b3224bc047fa2be6a54022b8f57aca9
Davis Hilton <davised2021@gmail.com>
2022-03-27 20:58:11 -0400

    donr neededm to work on null/blank catches

--- script.js
+++ script.js
@@ -2,45 +2,53 @@
 
 // 1) create an array with all possible character types: Uppercase, Lowercase, numbers, and symbols stored in a variable
 
-let characterTypes = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*(){}[]=<>,.ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-let arrayOfCharacterTypes = characterTypes.split('');  //turned string to an array
+const values =
+	"0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*(){}[]=<>,.ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+// let valuesArray = valuesString.split('');  //turned string to an array
 
-//console.log(arrayOfCharacterTypes);
-//================================================================================
+// console.log(valuesArray);
+//=================================================================
 
 // 2) create a global variable called "pwLength" with a number between 10 and 18
 
-let pwLength = 10;
+let pwLength = +prompt(
+	"How many charters would you like to use? The max character limit is 50. Once you enter the amount of charters click Generate."
+);
+//===============================================================
 
-//================================================================================
+// 3) Using the above array and password length variable, create a random password using aZI
+//    for loop inside of a function called "addNewPassword" either saved as an arrow function
+//    variable or a traditional function
 
-// 3) Using the above array and password length variable, create a random password using a    //    for loop inside of a function called "addNewPassword" either saved as an arrow function //    variable or a traditional function
-
-let addNewPassword = ()=>{
-    let password = '';
-    for(let i; i<=pwLength; i++){
-    let randomCharacter = Math.floor(Math.random() * arrayOfCharacterTypes.length);
-    password += password.push(randomCharacter[i]);
-    }
-    console.log(password)
-  } 
-    
-
-
+let addNewPassword = () => {
+	let password = "";
+	for (let i = 1; i <= pwLength; i++) {
+		if (pwLength <= 50) {
+			password += values.charAt(Math.floor(Math.random() * values.length - 1));
+		} else {
+			alert(
+				`You entered ${pwLength} which is ${
+					pwLength - 50
+				} over the max charater limit. Please try again.`
+			);
+			pwLength = +prompt(
+				"How many charters would you like to use? The max character limit is 50. Once you enter the amount of charters click Generate."
+			);
+		}
+	}
+	return `${password}`;
+};
 
 // ========= ⬇ DO NOT TOUCH THIS CODE ⬇ ===================================
 
 let genBtn = document.getElementById("btnGen");
 let buttonHandler = () => {
-    document.getElementById("password").value = addNewPassword();
+	document.getElementById("password").value = addNewPassword();
 };
 // Event listener for generate PW button
 genBtn.addEventListener("click", buttonHandler);
 
 // ========= ⬆ DO NOT TOUCH THIS CODE ⬆ ====================================
-
-
-
 
 //===================================================================
 //===============   Side note    ====================================
@@ -48,7 +56,7 @@ genBtn.addEventListener("click", buttonHandler);
 
 // 26 letters in alphabet but alphbet dont start on list until 97
 // so add 97 to get right starting point.
-//console.log(Math.floor(Math.random() * 26) + 97)  
+//console.log(Math.floor(Math.random() * 26) + 97)
 //==================================================================
 
 //Create an object of functions;
@@ -60,22 +68,22 @@ genBtn.addEventListener("click", buttonHandler);
 //     symbol : getRandomSymbol
 // };
 
-
+//CAN USE THESE FUNCTIONS TO GET VALUES
 
 // // Get random letter and make it lowere case or Upper case;
 // let getRandomLower= ()=>{
-//     return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+//     return String.fromCharCode(Math.floor(Math.random() * 26) + 97); 97 / start of lowercase
 // }
 // console.log(getRandomLower())
 
 // let getRandomUpper= ()=>{
-//     return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+//     return String.fromCharCode(Math.floor(Math.random() * 26) + 65); 65/ start of uppercase
 // }
 // console.log(getRandomUpper())
 
 // //Get Random Number
 // let getRandomNumber= ()=>{
-//     return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+//     return String.fromCharCode(Math.floor(Math.random() * 10) + 48); 48 start of numbers
 // }
 // console.log(getRandomNumber())
 
@@ -86,5 +94,11 @@ genBtn.addEventListener("click", buttonHandler);
 // }
 // console.log(getRandomSymbol())
 
+// let addNewPassword = () => {
+// 	let password = "";
 
-
+// 	for (let i = 1; i <= pwLength; i++) {
+// 		password += values.charAt(Math.floor(Math.random() * values.length - 1));
+// 	}
+// 	return `${password}`;
+// };
